075624e329bdbe442f3b70ad867652de46cd27cb
Prashant Nigam <prashant.nigam@rmtengg.com>
2024-09-16 18:00:04 +0530

    otp saved

--- src/component/OtpComponent.jsx
+++ src/component/OtpComponent.jsx
@@ -5,13 +5,20 @@ import "../assets/styles/login.css";
 import OtpInput from 'react-otp-input';
 import {Container, Row, Col, FloatingLabel} from 'react-bootstrap';
 import Spinner from 'react-bootstrap/Spinner';
+import { useNavigate } from 'react-router-dom';
 
 const OtpComponent = () => {
     const [otp, setOtp] = useState('');
+    const navigate = useNavigate()
    const handleOtpChange = (e) => {
     // console.log("e", e)
      setOtp(e)
    }
+
+   const handleSubmitOtp = () => {
+    navigate("/job-application-form-page")
+   }
+
     return (
         
         <div className='otp_box'>
@@ -26,7 +33,7 @@ const OtpComponent = () => {
                 />
 
                 <div className="d-grid">
-                    <Button size="lg" variant="primary" type="submit" className='Btn_Continue'>
+                    <Button size="lg" variant="primary" type="submit" onClick={handleSubmitOtp} className='Btn_Continue'>
                         Continue
                             {/* <Spinner animation="border" role="status">
                                 <span className="visually-hidden">Loading...</span>
