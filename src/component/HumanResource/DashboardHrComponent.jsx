a5852f540ff38ba0cd402d75602e88e1f5e94e7a
Prashant Nigam <prashant.nigam@rmtengg.com>
2024-09-23 19:09:49 +0530

    demo changes

--- src/component/HumanResource/DashboardHrComponent.jsx
+++ src/component/HumanResource/DashboardHrComponent.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {Container, Row, Col, Dropdown, Table, Form, Button } from 'react-bootstrap';
 import { FaRegUserCircle, FaSort } from "react-icons/fa";
 import { IoLogOutOutline } from "react-icons/io5";
-import { FiEdit } from "react-icons/fi";
+import { FiEdit, FiSearch } from "react-icons/fi";
 
 const DashboardHrComponent = () => {
     return (
@@ -76,19 +76,30 @@ const DashboardHrComponent = () => {
             </Row>
 
            
-            <Row className='mb-4'>
+            <Row className='mb-3'>
                 <Col>
                     <h4>Job Application  Details</h4>
                 </Col>
                 <Col className='text-end'>
                     <Form inline  className='text-end'>
                         <Row  className='text-end d-flex flex-row-reverse align-items-center'>
-                            <Col xs="auto">
+                            {/* <Col xs="auto">
                                 <Form.Control
                                 type="text"
                                 placeholder="Search"
                                 className=" mr-sm-2"
                                 />
+                            </Col> */}
+                            <Col className='text-end d-flex flex-row-reverse align-items-center'>
+                                <div className="appDate">
+                                    <Form.Control type="date" placeholder="date" />
+                                </div>
+                                <div class="input-group SearchBox">
+                                    <div class="input-group-prepend">
+                                        <button id="button-addon5" type="submit" class="btn btn-light"><FiSearch /></button>
+                                    </div>
+                                    <input type="search" placeholder="Search" aria-describedby="button-addon1" class="form-control border-0 bg-light" />
+                                </div>
                             </Col>
                         </Row>
                     </Form>
@@ -118,47 +129,47 @@ const DashboardHrComponent = () => {
                                 <td>prasahant@gmail.com</td>
                                 <td>Software Engineer</td>
                                 <td><span className='StatusTag notStartedTag'>Not Started</span></td>
-                                <td className='text-center EditCell'><FiEdit /></td>
+                                <td className='text-center EditCell'><span><FiEdit /></span></td>
                             </tr>
                             <tr>
                                 <td>2</td>
-                                <td>Prashant Nigam</td>
-                                <td>+91 9898989898</td>
-                                <td>23 Sept 2024</td>
-                                <td>prasahant@gmail.com</td>
-                                <td>Software Engineer</td>
+                                <td>Riya Sharma</td>
+                                <td>+91 9876543210</td>
+                                <td>15 Oct 2024</td>
+                                <td>riya.sharma@example.com</td>
+                                <td>Data Analyst</td>
                                 <td><span className='StatusTag ProgressTag'>In Progress</span></td>
-                                <td className='text-center EditCell'><FiEdit /></td>
+                                <td className='text-center EditCell'><span><FiEdit /></span></td>
                             </tr>
                             <tr>
                                 <td>3</td>
-                                <td>Prashant Nigam</td>
-                                <td>+91 9898989898</td>
-                                <td>23 Sept 2024</td>
-                                <td>prasahant@gmail.com</td>
-                                <td>Software Engineer</td>
+                                <td>Amit Verma</td>
+                                <td>+91 9988776655</td>
+                                <td>30 Nov 2024</td>
+                                <td>amit.verma@mail.com</td>
+                                <td>Project Manager</td>
                                 <td><span className='StatusTag CompletedTag'>Completed</span></td>
-                                <td className='text-center EditCell'><FiEdit /></td>
+                                <td className='text-center EditCell'><span><FiEdit /></span></td>
                             </tr>
                             <tr>
                                 <td>4</td>
-                                <td>Prashant Nigam</td>
-                                <td>+91 9898989898</td>
-                                <td>23 Sept 2024</td>
-                                <td>prasahant@gmail.com</td>
-                                <td>Software Engineer</td>
+                                <td>Sneha Patel</td>
+                                <td>+91 9123456789</td>
+                                <td>5 Dec 2024</td>
+                                <td>sneha.patel@example.com</td>
+                                <td>UX Designer</td>
                                 <td><span className='StatusTag notStartedTag'>Not Started</span></td>
-                                <td className='text-center EditCell'><FiEdit /></td>
+                                <td className='text-center EditCell'><span><FiEdit /></span></td>
                             </tr>
                             <tr>
                                 <td>5</td>
-                                <td>Prashant Nigam</td>
-                                <td>+91 9898989898</td>
-                                <td>23 Sept 2024</td>
-                                <td>prasahant@gmail.com</td>
-                                <td>Software Engineer</td>
+                                <td>Vikram Singh</td>
+                                <td>+91 9654321098</td>
+                                <td>12 Jan 2024</td>
+                                <td>vikram.singh@mail.com</td>
+                                <td>Web Developer</td>
                                 <td><span className='StatusTag notStartedTag'>Not Started</span></td>
-                                <td className='text-center EditCell'><FiEdit /></td>
+                                <td className='text-center EditCell'><span><FiEdit /></span></td>
                             </tr>
                         </tbody>
                     </Table>
